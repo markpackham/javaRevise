54302acdf3b3047b8f4d4377da7a30de853f18c2
Mark Packham <markpackham1@gmail.com>
2019-03-21 18:53:31 +0000

    use Java formatter, do class example & move onto control flow

---
+++ javaNotes.java
@@ -0,0 +1,444 @@
+//Hello World
+public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+    }
+  }
+
+
+//Terminal compile command (helps find errors) for files, "javac myFile.java"
+//It will create something like "myFile.class" to compile it type "java myFile" IGNORE the .class bit
+
+/*
+Java programs have at least one class and one main() method.
+Each class represents one real-world idea.
+The main() method runs the tasks of the program.
+Java comments add helpful context to human readers.
+Java has whitespace, curly braces, and semicolons.
+Whitespace is for humans to read code easily.
+Curly braces mark the scope of a class and method.
+Semicolons mark the end of a statement.
+Java is a compiled language.
+Compiling catches mistakes in our code.
+Compilers transform code into an executable class.
+*/
+
+//Simple Java example
+public class Review{
+    public static void main(String[] args) {
+        //The main method executes the tasks of the class
+      System.out.println("My first Java program from scratch!");
+      }
+  }
+
+
+//Variables
+public class Creator {
+	public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    System.out.println(name);
+    System.out.println(yearCreated);
+	}
+}
+
+
+//.equals() for strings
+public class Song {
+  public static void main(String[] args){
+    String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+    String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+    String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+    System.out.println(line1.equals(line2));
+    System.out.println(line2.equals(line3));
+  }       
+}
+
+//concatonate a string
+public class Zoo {
+  public static void main(String[] args){
+    int animals = 12;
+    String species = "zebra";
+    String zooDescription = "Our zoo has "+animals+" "+species+"s!";
+    System.out.println(zooDescription);
+  }       
+}
+
+
+//Class example
+public class Store {
+  // instance fields
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
+  
+  // constructor method
+  public Store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice = price;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade", 42, .99);
+    Store cookieShop = new Store("cookies", 12, 3.75);
+    
+    System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+    
+    System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+  }
+}
+
+
+//Declare instance
+public class Store {
+  // declare instance fields here!
+  String productType;
+  
+  // constructor method
+  public Store() {
+    System.out.println("I am inside the constructor method.");
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    System.out.println("This code is inside the main method.");
+    
+    Store lemonadeStand = new Store();
+    
+    System.out.println(lemonadeStand);
+  }
+}
+
+//Parameters & Arguments
+public class Store {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("lemonade");
+    System.out.println(lemonadeStand.productType);
+  }
+}
+
+
+//Another class example
+public class Dog {
+  String breed;
+  boolean hasOwner;
+  int age;
+  
+  public Dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Main method started");
+    Dog fido = new Dog("poodle", false, 4);
+    Dog nunzio = new Dog("shiba inu", true, 12);
+    boolean isFidoOlder = fido.age > nunzio.age;
+    int totalDogYears = nunzio.age + fido.age;
+    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
+    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
+    System.out.println("The total age of the dogs is: " + totalDogYears);
+    System.out.println("Main method finished");
+  }
+}
+
+
+//Methods example
+public class SavingsAccount {
+  
+  int balance;
+  
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
+  
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+    
+    //Check balance:
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+savings.balance);
+    
+    //Withdrawing:
+    int afterWithdraw = savings.balance - 300;
+    savings.balance = afterWithdraw;
+    System.out.println("You just withdrew "+300);
+    
+    //Check balance:
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+savings.balance);
+    
+    //Deposit:
+    int afterDeposit = savings.balance + 600;
+    savings.balance = afterDeposit;
+    System.out.println("You just deposited "+600);
+    
+    //Check balance:
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+savings.balance);
+    
+    //Deposit:
+    int afterDeposit2 = savings.balance + 600;
+    savings.balance = afterDeposit2;
+    System.out.println("You just deposited "+600);
+    
+    //Check balance:
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+savings.balance);
+    
+  }       
+}
+
+
+//call methods
+public class Store {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product) {
+    productType = product;
+  }
+  
+  // advertise method
+  public void advertise() {
+		System.out.println("Selling " + productType + "!");
+    System.out.println("Come spend some money!");
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade");
+    lemonadeStand.advertise();
+        lemonadeStand.advertise();
+    lemonadeStand.advertise();
+
+  }
+}
+
+//Reassigning Instance fields
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+  
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+  
+  // increase price method
+  public void increasePrice(double priceToAdd){
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+    lemonadeStand.increasePrice(1.5);
+    System.out.println(lemonadeStand.price);
+  }
+}
+
+//return example
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+  
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+  
+  // increase price method
+  public void increasePrice(double priceToAdd){
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+  }
+  
+  // get price with tax method
+public double getPriceWithTax(){
+  double totalPrice = price + price * 0.08;
+  return totalPrice;
+}
+  
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+double lemonadePrice = lemonadeStand.getPriceWithTax();
+    System.out.println(lemonadePrice);
+  }
+}
+
+
+//toString() is used in a class, we can return a String that will print when we print the object
+//otherwise we'd get something meanlingless like "Store@6bc7c054"
+public class Store {
+  // instance fields
+  String productType;
+  double price;
+  
+  // constructor method
+  public Store(String product, double initialPrice) {
+    productType = product;
+    price = initialPrice;
+  }
+  
+  // increase price method
+  public void increasePrice(double priceToAdd){
+    double newPrice = price + priceToAdd;
+    price = newPrice;
+  }
+  
+  // get price with tax method
+  public double getPriceWithTax(){
+    double tax = 0.08;
+    double totalPrice = price + price*tax;
+    return totalPrice;
+  }
+  
+  public String toString(){
+    return "This store sells " + productType + " at a price of " + price + ".";
+  }
+
+  // main method
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+    Store cookieShop = new Store("Cookies", 5);
+
+    System.out.println(lemonadeStand);
+    System.out.println(cookieShop);
+  }
+}
+
+/*
+Defining a method : Methods have a method signature that declares their return type, name, and parameters
+Calling a method : Methods are invoked with a . and ()
+Parameters : Inputs to the method and their types are declared in parentheses in the method signature
+Changing Instance Fields : Methods can be used to change the value of an instance field
+Scope : Variables only exist within the domain that they are created in
+Return : The type of the variables that are output are declared in the method signature
+*/
+
+//Bank class example
+public class SavingsAccount {
+  
+  int balance;
+  
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
+  
+  public void checkBalance(){
+    System.out.println("Hello!");
+    System.out.println("Your balance is "+balance);
+  }
+  
+  public void deposit(int amountToDeposit){
+    balance = amountToDeposit + balance;
+    System.out.println("You just deposited " + amountToDeposit);
+  }
+  
+  public int withdraw(int amountToWithdraw){
+    balance = balance - amountToWithdraw;
+    System.out.println("You just withdrew " + amountToWithdraw);
+    return amountToWithdraw;
+  }
+  
+  public String toString(){
+    return "This is a savings account with " + balance + " saved.";
+  }
+  
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Withdrawing:
+    savings.withdraw(300);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Deposit:
+    savings.deposit(600);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Deposit:
+    savings.deposit(600);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    System.out.println(savings);
+  }       
+}
+
+
+//Control flow example
+public class Conditionals {
+	public static void main(String[] args) {
+
+
+		if (1 < 4 && 0 > 5) {
+
+			System.out.println("You ordered a cup of hot, mint tea.");
+
+		} else if (21 <= 19 || 17 >= 28) {
+			
+			System.out.println("You ordered freshly squeezed orange juice!");
+
+		} else if ( !(true == true) ) {
+
+			System.out.println("You ordered hot cocoa!");
+
+		} else {
+
+			System.out.println("You ordered a cup of Java!");
+
+		}
+
+		char answerChoice = 'C';
+
+		switch (answerChoice) {
+
+			case 'A': System.out.println("You answered: " + answerChoice + ". Please try again.");
+								break; 
+
+			case 'B': System.out.println("You answered: " + answerChoice + ". Please try again.");
+								break;
+
+			case 'C': System.out.println("You answered: " + answerChoice + ". That is correct!");
+								break;
+
+			case 'D': System.out.println("You answered: " + answerChoice + ". Please try again.");
+								break;
+
+			default:
+				System.out.println("Please select a valid answer choice.");
+
+		}
+
+
+	}
+}
